6131aee803e5ab979bbd042cc9c7ef02ce87f66a
Rajath Kumar D <acharya.drajath@gmail.com>
2025-11-08 17:56:01 +0530

    feat: refactor login and home pages to use SplashBackground component for improved layout

---
+++ kpl-ui/components/SplashBackground.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SplashBackground: React.FC = () => {
+  return (
+    <div className="home-background">
+      <div className="splash splash-1"></div>
+      <div className="splash splash-2"></div>
+      <div className="splash splash-3"></div>
+      <div className="splash splash-4"></div>
+    </div>
+  );
+};
+
+export default SplashBackground;
