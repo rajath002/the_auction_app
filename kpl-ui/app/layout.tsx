623e0812ccd476aca9269a3725c581e6f6911098
Rajath Kumar D <acharya.drajath@gmail.com>
2025-12-01 23:52:25 +0530

    feat: modernize styling by replacing globals.scss with index.scss and adding home-background.scss

--- kpl-ui/app/layout.tsx
+++ kpl-ui/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
-import "./globals.scss";
+import "../styles/index.scss";
 import Header from "@/components/header";
 import { AppProvider } from "@/context/useAppState";
 import PageLoader from "@/components/PageLoader";
